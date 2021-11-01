5e538ba57e9edf28104e1db7d80f3a81df0dc157
jeevan-kishore <kishor080@gmail.com>
2021-11-02 02:19:50 +0530

    char escape

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ const getTopPerformers = (cryptoListObject) => {
       acc.push({
         coin: ce,
         valueChange: changeValue,
-        displayValue: `<strong>${ce}:</strong><i>${changeValue}%</i>`,
+        displayValue: `\<strong\>${ce}:\</strong\>\<i\>${changeValue}%\</i\>`,
       });
       return acc;
     }, [])
@@ -34,7 +34,7 @@ const getLatestPrice = () => {
       performers
         .map((item) => item.displayValue)
         .slice(0, 50)
-        .join(" <br /> ")
+        .join(" \<br /\> ")
     );
   });
 };
