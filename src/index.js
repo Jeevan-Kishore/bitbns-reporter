5505b476a5c7caae6dc73b2bdca7f8c2e4574314
jeevan-kishore <kishor080@gmail.com>
2021-11-02 00:21:29 +0530

    rewrite triggers

--- src/index.js
+++ src/index.js
@@ -1,36 +1,28 @@
-const { TelegramClient } = require('messaging-api-telegram');
-const bitbnsApi = require('bitbns');
+const { TelegramClient } = require("messaging-api-telegram");
+const bitbnsApi = require("bitbns");
 
-const initiateBNS = () => {
-    return new bitbnsApi({
-        apiKey: process.env.BIT_BNS_API_KEY,
-        apiSecretKey: process.env.BIT_BNS_SECRET_KEY
-    });
-}
+const bitbns = new bitbnsApi({
+  apiKey: process.env.BIT_BNS_API_KEY,
+  apiSecretKey: process.env.BIT_BNS_SECRET_KEY,
+});
 
-const initiateTelegramClient = () => {
-    return new TelegramClient({
-        accessToken: process.env.TELEGRAM_API_KEY,
-    });
-}
+const client = TelegramClient({
+  accessToken: process.env.TELEGRAM_API_KEY,
+});
 
-const getLatestPrice = (bitbns) => {
-    bitbns.getTickerApi('BTC,ETH',(error,data) => {
-        console.log("BTC Price :: ",data);
-    })
-}
+const getLatestPrice = () => {
+  bitbns.getTickerApi("BTC,ETH", (error, data) => {
+    sendOutMessage(data);
+  });
+};
 
-const sendOutMessage = async (client) => {
-    await client.sendMessage(1050401913, `Hello World @ ${new Date()}`);
-    console.log('<-- sent message -->');
-}
+const sendOutMessage = (message) => {
+   client.sendMessage(1050401913, message);
+};
 
 const main = () => {
-    const bitbns = initiateBNS();
-    const client = initiateTelegramClient();
-    getLatestPrice(bitbns);
-    sendOutMessage(client);
-}
-
+  getLatestPrice(bitbns);
+  sendOutMessage(client);
+};
 
 main();
